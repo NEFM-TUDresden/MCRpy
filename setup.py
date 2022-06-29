463e40bfeb1b9a375bd919d01d00b3dcf59323ce
Paul Seibert <paul@paul.mw.tu-dresden.de>
2022-06-29 16:37:29 +0200

    std version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='mcrpy',
-      version='0.1',
+      version='0.1.0',
       description='Microstructure characterization and reconstruction in Python',
       url='https://github.com/NEFM-TUDresden/MCRpy',
       author='NEFM-TUDresden',
