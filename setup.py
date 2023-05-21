78fbcce8798537272e6f397fa6bea2fe2843eea8
Paul Seibert <paul.seibert@tu-dresden.de>
2023-05-21 10:47:28 +0200

    change dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='mcrpy',
-      version='0.1.0',
+      version='0.2.0',
       description='Microstructure characterization and reconstruction in Python',
       url='https://github.com/NEFM-TUDresden/MCRpy',
       author='NEFM-TUDresden',
@@ -14,15 +14,14 @@ setup(name='mcrpy',
       packages=find_packages(),
       install_requires=[
           'numpy>=1.20.1',
-          'tensorflow>=2.3.1',
+          'tensorflow>=2.7.1',
           'matplotlib>=3.3.4',
-          'scipy>=1.6.2'
+          'scipy>=1.6.2',
+          'pyevtk',
       ],
       extras_require={
-          'FFTCorrelations': ['pymks>=0.4.1', 'sklearn>=0.24.1'],
           'animations': ['imageio-ffmpeg>=0.4.5'],
           'GUI': ['Gooey>=1.0.8.1']
           },
       package_data={'': ['*.pkl', '*.npy', '*.pickle', '*.png']},
       zip_safe=False)
-
