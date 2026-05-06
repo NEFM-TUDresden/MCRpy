f47dc53035c6176442393c7c903e7d79763ef955
Alexander Rassloff <alexander.rassloff@tu-dresden.de>
2026-05-06 14:06:02 +0200

    Adapt to new default shape dtype of tensorflow; np.prodcut --> np.prod

--- mcrpy/losses/Loss.py
+++ mcrpy/losses/Loss.py
@@ -1,20 +1,21 @@
 """
-   Copyright 10/2020 - 04/2021 Paul Seibert for Diploma Thesis at TU Dresden
-   Copyright 05/2021 - 12/2021 TU Dresden (Paul Seibert as Scientific Assistant)
-   Copyright 2022 TU Dresden (Paul Seibert as Scientific Employee)
+Copyright 10/2020 - 04/2021 Paul Seibert for Diploma Thesis at TU Dresden
+Copyright 05/2021 - 12/2021 TU Dresden (Paul Seibert as Scientific Assistant)
+Copyright 2022 TU Dresden (Paul Seibert as Scientific Employee)
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
+
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
@@ -33,59 +34,70 @@ class Loss(ABC):
         pass
 
     @classmethod
-    def make_loss(cls,
-                  descriptor_list: List[callable] = None,
-                  desired_descriptor_list: List[np.ndarray] = None,
-                  descriptor_weights: List[float] = None,
-                  descriptor_comparisons: List[callable] = None,
-                  dropout_rate: float = None, # if you change this you better know what you are doing
-                  tvd: float = None,
-                  oor_multiplier: float = 1000.0,
-                  phase_sum_multiplier: float = 1.0,
-                  mg_level: int = None,
-                  mg_levels: int = None,
-                  desired_shape_extended: Tuple[int] = None,
-                  penalize_uncertainty: bool = False,
-                  use_multiphase: bool = False,
-                  descriptor_is_multiphase: List[bool] = None,
-                  use_orientations: bool = False,
-                  **kwargs) -> callable:
+    def make_loss(
+        cls,
+        descriptor_list: List[callable] = None,
+        desired_descriptor_list: List[np.ndarray] = None,
+        descriptor_weights: List[float] = None,
+        descriptor_comparisons: List[callable] = None,
+        dropout_rate: float = None,  # if you change this you better know what you are doing
+        tvd: float = None,
+        oor_multiplier: float = 1000.0,
+        phase_sum_multiplier: float = 1.0,
+        mg_level: int = None,
+        mg_levels: int = None,
+        desired_shape_extended: Tuple[int] = None,
+        penalize_uncertainty: bool = False,
+        use_multiphase: bool = False,
+        descriptor_is_multiphase: List[bool] = None,
+        use_orientations: bool = False,
+        **kwargs,
+    ) -> callable:
 
         if descriptor_list is None:
-            raise ValueError('A descriptor_list must be passed that is not None.')
+            raise ValueError("A descriptor_list must be passed that is not None.")
         if desired_descriptor_list is None:
-            raise ValueError('A desired_descriptor_list must be passed that is not None.')
+            raise ValueError("A desired_descriptor_list must be passed that is not None.")
         if descriptor_weights is None:
-            raise ValueError('Some descriptor_weights must be passed that is not None.')
+            raise ValueError("Some descriptor_weights must be passed that is not None.")
         if descriptor_comparisons is None:
-            raise ValueError('Some descriptor_comparisons must be passed that is not None.')
+            raise ValueError("Some descriptor_comparisons must be passed that is not None.")
         if not (len(descriptor_list) == len(desired_descriptor_list) == len(descriptor_weights)):
             raise ValueError(
                 (
-                    'The lengths of the descriptor_list, desired_descriptor_list '
-                    + f'and descriptor_weights are {len(descriptor_list)}, '
-                    + f'{len(desired_descriptor_list)} and {len(descriptor_weights)} '
-                    + 'respectively, but should be all the same.'
+                    "The lengths of the descriptor_list, desired_descriptor_list "
+                    + f"and descriptor_weights are {len(descriptor_list)}, "
+                    + f"{len(desired_descriptor_list)} and {len(descriptor_weights)} "
+                    + "respectively, but should be all the same."
                 )
             )
 
         if dropout_rate is not None:
-            num_samples = tf.constant(sum(np.product(desired_descriptor.shape) 
-                for desired_descriptor in desired_descriptor_list) * ((mg_level + 1) / desired_descriptor_list[0].shape[1]), dtype=tf.int32)
+            num_samples = tf.constant(
+                sum(np.prod(desired_descriptor.shape) for desired_descriptor in desired_descriptor_list)
+                * ((mg_level + 1) / desired_descriptor_list[0].shape[1]),
+                dtype=tf.int32,
+            )
 
             @tf.function
             def draw_kept_components() -> tf.Tensor:
-                component_mask = tf.cast(tf.random.categorical(tf.math.log(
-                    [[dropout_rate, 1 - dropout_rate]]), num_samples, seed=1), tf.float64)
+                component_mask = tf.cast(
+                    tf.random.categorical(tf.math.log([[dropout_rate, 1 - dropout_rate]]), num_samples, seed=1),
+                    tf.float64,
+                )
                 if tf.norm(component_mask, ord=1) < 0.5:
-                    component_mask = tf.cast(tf.random.categorical(tf.math.log(
-                        [[dropout_rate, 1 - dropout_rate]]), num_samples, seed=1), tf.float64)
+                    component_mask = tf.cast(
+                        tf.random.categorical(tf.math.log([[dropout_rate, 1 - dropout_rate]]), num_samples, seed=1),
+                        tf.float64,
+                    )
                 return component_mask
 
         if tvd is not None and tvd > 0:
+
             @tf.function
             def tvd_term(x):
                 return tf.image.total_variation(x)
+
         else:
             tvd_term = None
 
@@ -94,7 +106,9 @@ class Loss(ABC):
         phases = list(range(n_phases))
         phases_desired = phases
         if desired_descriptor_list[0].shape[0] > n_phases:
-            logging.warning('desired_descriptor has multiphase, but multiphase is switched off for reconstruction. Using phase 1')
+            logging.warning(
+                "desired_descriptor has multiphase, but multiphase is switched off for reconstruction. Using phase 1"
+            )
             phases_desired = [1]
         n_mg_levels = min(desired_descriptor_list[0].shape[1], mg_levels)
         n_pixels = np.prod(desired_shape_extended[:-1])
@@ -106,8 +120,19 @@ class Loss(ABC):
         @tf.function
         def compute_energy(x):
             flattened_difference_list = []
-            for descriptor_function, desired_descriptor, descriptor_weight, descriptor_comparison, d_is_multiphase in zip(
-                    descriptor_list, desired_descriptor_list, descriptor_weights, descriptor_comparisons, descriptor_is_multiphase):
+            for (
+                descriptor_function,
+                desired_descriptor,
+                descriptor_weight,
+                descriptor_comparison,
+                d_is_multiphase,
+            ) in zip(
+                descriptor_list,
+                desired_descriptor_list,
+                descriptor_weights,
+                descriptor_comparisons,
+                descriptor_is_multiphase,
+            ):
                 current_descriptor = descriptor_function(x)
                 desired_descriptor = tf.cast(tf.constant(desired_descriptor), tf.float64)
                 descriptor_differences = []
@@ -124,12 +149,23 @@ class Loss(ABC):
                         phase_weight = 1.0
                     for n_mg_level in range(n_mg_levels - mg_level):
                         level_weight = 2**n_mg_level
-                        descriptor_differences.append(tf.reshape( 
-                            phase_weight * level_weight * descriptor_comparison(
-                                current_descriptor[phase, n_mg_level], 
-                                desired_descriptor[phase_desired, n_mg_level + mg_level]), [1, -1]))
+                        descriptor_differences.append(
+                            tf.reshape(
+                                phase_weight
+                                * level_weight
+                                * descriptor_comparison(
+                                    current_descriptor[phase, n_mg_level],
+                                    desired_descriptor[phase_desired, n_mg_level + mg_level],
+                                ),
+                                [1, -1],
+                            )
+                        )
                 try:
-                    weighted_descriptor_difference = tf.concat(descriptor_differences, axis=0) if len(descriptor_differences) > 1 else descriptor_differences[0]
+                    weighted_descriptor_difference = (
+                        tf.concat(descriptor_differences, axis=0)
+                        if len(descriptor_differences) > 1
+                        else descriptor_differences[0]
+                    )
                     weighted_descriptor_difference = tf.reshape(weighted_descriptor_difference, [-1])
                 except IndexError as e:
                     raise ValueError("""Combination of multigrid settings and limit_to not 
@@ -137,15 +173,17 @@ class Loss(ABC):
                             use_multigrid_descriptor, use_multigrid_reconstruction or 
                             limit_to or change the characterization settings.""")
                 flattened_difference_list.append(weighted_descriptor_difference * descriptor_weight)
-            difference = tf.concat(flattened_difference_list, 0) if len(flattened_difference_list) > 1 else flattened_difference_list[0]
+            difference = (
+                tf.concat(flattened_difference_list, 0)
+                if len(flattened_difference_list) > 1
+                else flattened_difference_list[0]
+            )
             if dropout_rate is None:
                 energy = norm(difference)
                 return energy
             else:
                 component_mask = draw_kept_components()
-                difference_mb = tf.math.multiply(
-                        difference, 
-                        tf.reshape(component_mask,tf.shape(difference)))
+                difference_mb = tf.math.multiply(difference, tf.reshape(component_mask, tf.shape(difference)))
                 energy = norm(difference_mb)
                 return energy
 
@@ -161,8 +199,7 @@ class Loss(ABC):
 
         @tf.function
         def compute_oor_penalty(x):
-            out_of_range = tf.math.square(tf.nn.relu(
-                x - 1) + tf.nn.relu(0 - x))
+            out_of_range = tf.math.square(tf.nn.relu(x - 1) + tf.nn.relu(0 - x))
             penalty_oor = tf.math.reduce_mean(out_of_range)
             return penalty_oor
 
--- mcrpy/src/MutableMicrostructure.py
+++ mcrpy/src/MutableMicrostructure.py
@@ -8,38 +8,56 @@ from typing import Dict, List, Tuple, Union
 import numpy as np
 from scipy.ndimage import convolve
 import tensorflow as tf
+
 # tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 from mcrpy.src.Symmetry import Symmetry, Cubic
 from mcrpy.src.Microstructure import Microstructure
 
+
 class MutableMicrostructure(Microstructure):
 
-    def __init__(self, 
-            array: np.ndarray, 
-            use_multiphase = False,
-            ori_repr: type = None,
-            symmetry: Symmetry = Cubic,
-            skip_encoding: bool = False,
-            trainable: bool = True):
+    def __init__(
+        self,
+        array: np.ndarray,
+        use_multiphase=False,
+        ori_repr: type = None,
+        symmetry: Symmetry = Cubic,
+        skip_encoding: bool = False,
+        trainable: bool = True,
+    ):
         super().__init__(
             array,
-            use_multiphase = use_multiphase,
-            ori_repr = ori_repr,
-            symmetry = symmetry,
-            skip_encoding = skip_encoding,
-            trainable = trainable)
+            use_multiphase=use_multiphase,
+            ori_repr=ori_repr,
+            symmetry=symmetry,
+            skip_encoding=skip_encoding,
+            trainable=trainable,
+        )
 
-        self.linear_indices = np.array(list(range(np.product(self.spatial_shape))))
+        self.linear_indices = np.array(list(range(np.prod(self.spatial_shape))))
         if self.is_3D:
-            self.conv_weights = np.array([[[0, 0,  0], [0, -1, 0], [0, 0, 0]], [[0, -1, 0], [-1, 6, -1], [0, -1, 0]], [[0, 0, 0], [0, -1, 0], [0, 0, 0]]], dtype=np.int32)
-            self.neighbor_conv_weights = np.array([[[0, 0,  0], [0, 1, 0], [0, 0, 0]], [[0, 1, 0], [1, 0, 1], [0, 1, 0]], [[0, 0, 0], [0, 1, 0], [0, 0, 0]]], dtype=np.int32)
+            self.conv_weights = np.array(
+                [
+                    [[0, 0, 0], [0, -1, 0], [0, 0, 0]],
+                    [[0, -1, 0], [-1, 6, -1], [0, -1, 0]],
+                    [[0, 0, 0], [0, -1, 0], [0, 0, 0]],
+                ],
+                dtype=np.int32,
+            )
+            self.neighbor_conv_weights = np.array(
+                [
+                    [[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+                    [[0, 1, 0], [1, 0, 1], [0, 1, 0]],
+                    [[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+                ],
+                dtype=np.int32,
+            )
         else:
             self.conv_weights = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]], dtype=np.int32)
             self.neighbor_conv_weights = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=np.int32)
         self.pre_mutation = None
         self.post_mutation = None
-
 
     def __repr__(self):
         representation = super().__repr__()
@@ -52,21 +70,22 @@ class MutableMicrostructure(Microstructure):
             return representation
 
     def sample_phase_location(
-            self,
-            field: np.ndarray,
-            phase: int,
-            neighbor_offset: float = 2.9,
-            multiple: int = None,
-            neighbor_in_phase: Union[int, None] = None):
-        conv_result = convolve(field[0,...,phase], self.conv_weights, mode='wrap').flatten()
+        self,
+        field: np.ndarray,
+        phase: int,
+        neighbor_offset: float = 2.9,
+        multiple: int = None,
+        neighbor_in_phase: Union[int, None] = None,
+    ):
+        conv_result = convolve(field[0, ..., phase], self.conv_weights, mode="wrap").flatten()
         if neighbor_in_phase is not None:
-            conv_result = conv_result * convolve(
-                    field[0, ..., neighbor_in_phase],
-                    self.neighbor_conv_weights,
-                    mode='wrap').flatten()
-        clipped_result = np.clip(conv_result-neighbor_offset, 0, 10)
+            conv_result = (
+                conv_result
+                * convolve(field[0, ..., neighbor_in_phase], self.neighbor_conv_weights, mode="wrap").flatten()
+            )
+        clipped_result = np.clip(conv_result - neighbor_offset, 0, 10)
         unscaled_probs = clipped_result**2
-        assert np.sum(unscaled_probs>0)>0 , "no pixel remaining"
+        assert np.sum(unscaled_probs > 0) > 0, "no pixel remaining"
         probs = unscaled_probs / np.sum(unscaled_probs)
         chosen_linear_index = np.random.choice(self.linear_indices, multiple, replace=False, p=probs)
         if multiple in {0, 1, None}:
@@ -74,11 +93,11 @@ class MutableMicrostructure(Microstructure):
         else:
             return [np.unravel_index(cli, field.shape[:-1]) for cli in chosen_linear_index]
 
-    def mutate(self, n_attempts: int = 1000, rule: str = 'relaxed_neighbor'):
+    def mutate(self, n_attempts: int = 1000, rule: str = "relaxed_neighbor"):
         self.pre_mutation = tf.identity(self.x)
         with self.use_multiphase_encoding() as x:
             mutant = x.numpy()
-            if rule == 'random':
+            if rule == "random":
                 index_1 = tuple([0] + [random.randint(0, s_i - 1) for s_i in self.spatial_shape])
                 phases_1 = np.argmax(mutant[index_1])
                 for n_attempt in range(n_attempts):
@@ -87,29 +106,32 @@ class MutableMicrostructure(Microstructure):
                     if np.sum(np.square(phases_1 - phases_2)) > 1e-9:
                         break
                 else:
-                    logging.warning('reached max number of mutation attempts')
-            elif rule == 'relaxed_neighbor':
+                    logging.warning("reached max number of mutation attempts")
+            elif rule == "relaxed_neighbor":
                 swap_phases = random.sample(list(range(max(2, self.n_phases))), 2)
                 swap_indices = [self.sample_phase_location(mutant, phase, neighbor_offset=0) for phase in swap_phases]
                 index_1, index_2 = swap_indices
                 phases_1, phases_2 = swap_phases
-            elif rule == 'neighbor':
+            elif rule == "neighbor":
                 swap_phases = random.sample(list(range(max(2, self.n_phases))), 2)
-                swap_indices = [self.sample_phase_location(mutant, phase, neighbor_offset=2.9 if self.is_3D else 1.9) for phase in swap_phases]
+                swap_indices = [
+                    self.sample_phase_location(mutant, phase, neighbor_offset=2.9 if self.is_3D else 1.9)
+                    for phase in swap_phases
+                ]
                 index_1, index_2 = swap_indices
                 phases_1, phases_2 = swap_phases
-            elif rule == 'neighbor_in_phase':
+            elif rule == "neighbor_in_phase":
                 swap_phases = random.sample(list(range(max(2, self.n_phases))), 2)
                 phases_1, phases_2 = swap_phases
-                neighbor_offset=2.9 if self.is_3D else 1.9
+                neighbor_offset = 2.9 if self.is_3D else 1.9
                 index_1 = self.sample_phase_location(
-                        mutant, phases_1, neighbor_offset=neighbor_offset, 
-                        neighbor_in_phase=phases_2)
+                    mutant, phases_1, neighbor_offset=neighbor_offset, neighbor_in_phase=phases_2
+                )
                 index_2 = self.sample_phase_location(
-                        mutant, phases_2, neighbor_offset=neighbor_offset, 
-                        neighbor_in_phase=phases_1)
+                    mutant, phases_2, neighbor_offset=neighbor_offset, neighbor_in_phase=phases_1
+                )
             else:
-                raise NotImplementedError(f'Mutation rule {rule} is not implemented')
+                raise NotImplementedError(f"Mutation rule {rule} is not implemented")
             mutant[index_1][phases_1] = 0
             mutant[index_1][phases_2] = 1
             mutant[index_2][phases_2] = 0
@@ -118,7 +140,7 @@ class MutableMicrostructure(Microstructure):
                 self.swapped_index_1.assign(np.array(index_1[1:], dtype=np.int32))
                 self.swapped_index_2.assign(np.array(index_2[1:], dtype=np.int32))
             x.assign(mutant)
-            
+
     def redo_mutation(self):
         assert self.post_mutation is not None
         self.x.assign(self.post_mutation)
@@ -130,6 +152,7 @@ class MutableMicrostructure(Microstructure):
 
     def adjust_vf(self, volume_fractions: np.ndarray):
         """Rounds values and makes sure the volume fractions are met."""
+
         def get_state_probability(field: np.ndarray, state: int) -> float:
             """Get the probability of state."""
             indicator_function = field[..., int(round(state))]
@@ -144,14 +167,14 @@ class MutableMicrostructure(Microstructure):
         #     chosen_possibility = random.randint(0, n_possibilities - 1)
         #     return tuple([location_1d[chosen_possibility] for location_1d in possible_locations])
 
-        logging.info('start adjusting volume fractions')
+        logging.info("start adjusting volume fractions")
         neighbor_offset = 2.9
         with self.use_multiphase_encoding() as x:
             if isinstance(volume_fractions, tuple):
                 volume_fractions = volume_fractions[0]
             volume_fractions = volume_fractions[:, 0].flatten()
             if len(volume_fractions) == 1:
-                volume_fractions = np.array([1-volume_fractions[0], volume_fractions[0]])
+                volume_fractions = np.array([1 - volume_fractions[0], volume_fractions[0]])
             field = np.round(np.copy(x.numpy()), decimals=0).astype(np.int32)
             init_fails = np.where(np.sum(field, axis=-1) != 1)
             for init_fail in zip(*init_fails):
@@ -160,22 +183,31 @@ class MutableMicrostructure(Microstructure):
             previous_loss = np.inf
             n_phases = len(volume_fractions)
             while True:
-                state_probabilities = np.array([get_state_probability(field, state) 
-                    for state in range(n_phases)])
+                state_probabilities = np.array([get_state_probability(field, state) for state in range(n_phases)])
                 vf_delta = state_probabilities - volume_fractions
-                new_loss = np.sum((vf_delta)**2)
+                new_loss = np.sum((vf_delta) ** 2)
                 if new_loss >= previous_loss:
                     break
                 phase_over = np.where(vf_delta == max(vf_delta))[0][0]
                 phase_under = np.where(vf_delta == min(vf_delta))[0][0]
-                excess = int(max(vf_delta[phase_over], np.abs(vf_delta[phase_under])) * np.product(self.spatial_shape) / 10)
+                excess = int(
+                    max(vf_delta[phase_over], np.abs(vf_delta[phase_under])) * np.prod(self.spatial_shape) / 10
+                )
                 multiple = excess if excess >= 2 else None
-                # multiple = 10 if vf_delta[phase_over] * np.product(self.spatial_shape) > 10 else None
-                print(f'{volume_fractions} - {state_probabilities} = {vf_delta} - loss {new_loss} - {phase_under} from {phase_over}')
+                # multiple = 10 if vf_delta[phase_over] * np.prod(self.spatial_shape) > 10 else None
+                print(
+                    f"{volume_fractions} - {state_probabilities} = {vf_delta} - loss {new_loss} - {phase_under} from {phase_over}"
+                )
                 if neighbor_offset < 0:
                     raise ValueError
                 try:
-                    place_over = self.sample_phase_location(field, phase_over, neighbor_offset=neighbor_offset, multiple=multiple, neighbor_in_phase=phase_under)
+                    place_over = self.sample_phase_location(
+                        field,
+                        phase_over,
+                        neighbor_offset=neighbor_offset,
+                        multiple=multiple,
+                        neighbor_in_phase=phase_under,
+                    )
                 except (AssertionError, ValueError):
                     neighbor_offset -= 0.5
                     continue
@@ -188,4 +220,4 @@ class MutableMicrostructure(Microstructure):
                         field[p_o][phase_under] = 1
                 previous_loss = new_loss
             x.assign(field)
-        logging.info('done adjusting volume fractions')
+        logging.info("done adjusting volume fractions")
