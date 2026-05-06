ba86e3667ff1ffc37bd8e443c9aff43c87525a61
Alexander Rassloff <alexander.rassloff@tu-dresden.de>
2026-05-06 13:57:34 +0200

    Take real part of fourier coefficients prior to casting to avoid tensorflow warnings

--- mcrpy/descriptors/FFTCorrelations.py
+++ mcrpy/descriptors/FFTCorrelations.py
@@ -1,20 +1,21 @@
 """
-   Copyright 10/2020 - 04/2021 Paul Seibert for Diploma Thesis at TU Dresden
-   Copyright 05/2021 - 12/2021 TU Dresden (Paul Seibert as Scientific Assistant)
-   Copyright 2022 TU Dresden (Paul Seibert as Scientific Employee)
+Copyright 10/2020 - 04/2021 Paul Seibert for Diploma Thesis at TU Dresden
+Copyright 05/2021 - 12/2021 TU Dresden (Paul Seibert as Scientific Assistant)
+Copyright 2022 TU Dresden (Paul Seibert as Scientific Employee)
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
+
 from __future__ import annotations
 from typing import Tuple
 
@@ -23,24 +24,24 @@ import tensorflow as tf
 
 from mcrpy.src import descriptor_factory
 from mcrpy.descriptors.PhaseDescriptor import PhaseDescriptor
+from mcrpy.src import log
+
+tf.get_logger().setLevel(log.logging.ERROR)
 
 
 class FFTCorrelations(PhaseDescriptor):
     is_differentiable = True
 
     @staticmethod
-    def make_singlephase_descriptor(
-            desired_shape_2d=(64, 64), 
-            limit_to: int = None, 
-            **kwargs) -> callable:
+    def make_singlephase_descriptor(desired_shape_2d=(64, 64), limit_to: int = None, **kwargs) -> callable:
 
         mask = np.zeros(desired_shape_2d)
         i_center = desired_shape_2d[0] // 2
         j_center = desired_shape_2d[1] // 2
-        mask[i_center - limit_to + 1:i_center+limit_to, j_center - limit_to + 1:j_center+limit_to] = 1
+        mask[i_center - limit_to + 1 : i_center + limit_to, j_center - limit_to + 1 : j_center + limit_to] = 1
         mask = mask.astype(bool)
         mask = tf.constant(mask, dtype=tf.bool)
-        descriptor_shape = (limit_to * 2 - 1,) *  2
+        descriptor_shape = (limit_to * 2 - 1,) * 2
 
         @tf.function
         def compute_descriptor(microstructure: tf.Tensor) -> tf.Tensor:
@@ -50,15 +51,16 @@ class FFTCorrelations(PhaseDescriptor):
             fourier_coefficients = ms_fourier * ms_conj / tf.cast(tf.size(microstructure), tf.complex128)
             real_coefficients = tf.signal.ifft2d(fourier_coefficients)
             real_coefficients = tf.signal.fftshift(real_coefficients)
-            selected_components = tf.reshape(tf.boolean_mask(tf.cast(real_coefficients, tf.float64), mask), descriptor_shape)
+            real_coefficients_real = tf.math.real(real_coefficients)
+            real_coefficients_real = tf.cast(real_coefficients_real, tf.float64)
+            selected_components = tf.reshape(tf.boolean_mask(real_coefficients_real, mask), descriptor_shape)
+
             return selected_components
+
         return compute_descriptor
 
     @staticmethod
-    def define_comparison_mask(
-            desired_descriptor_shape: Tuple[int] = None, 
-            limit_to: int = None, 
-            **kwargs):
+    def define_comparison_mask(desired_descriptor_shape: Tuple[int] = None, limit_to: int = None, **kwargs):
         assert len(desired_descriptor_shape) == 2
         assert desired_descriptor_shape[1] == desired_descriptor_shape[0]
         desired_limit_to = desired_descriptor_shape[1] // 2 + 1
@@ -76,21 +78,17 @@ class FFTCorrelations(PhaseDescriptor):
 
     @classmethod
     def visualize_subplot(
-            cls,
-            descriptor_value: np.ndarray,
-            ax,
-            descriptor_type: str = None,
-            mg_level: int = None,
-            n_phase: int = None):
+        cls, descriptor_value: np.ndarray, ax, descriptor_type: str = None, mg_level: int = None, n_phase: int = None
+    ):
         s2 = descriptor_value[:, :]
         height, width = s2.shape
         if height != width:
-            raise NotImplementedError('Non-square FFTCorrelations not implemented')
+            raise NotImplementedError("Non-square FFTCorrelations not implemented")
         limit_to = height // 2 + 1
-        ax.imshow(s2, cmap='cividis')
-        ax.set_title(f'S2: l={mg_level}, p={n_phase}')
-        ax.set_xlabel(r'$r_x$ in Px')
-        ax.set_ylabel(r'$r_y$ in Px')
+        ax.imshow(s2, cmap="cividis")
+        ax.set_title(f"S2: l={mg_level}, p={n_phase}")
+        ax.set_xlabel(r"$r_x$ in Px")
+        ax.set_ylabel(r"$r_y$ in Px")
         xticks = [0, limit_to - 1, 2 * (limit_to - 1)]
         yticks = [0, limit_to - 1, 2 * (limit_to - 1)]
         ax.set_xticks(xticks)
--- mcrpy/descriptors/FFTCrossCorrelations.py
+++ mcrpy/descriptors/FFTCrossCorrelations.py
@@ -1,20 +1,21 @@
 """
-   Copyright 10/2020 - 04/2021 Paul Seibert for Diploma Thesis at TU Dresden
-   Copyright 05/2021 - 12/2021 TU Dresden (Paul Seibert as Scientific Assistant)
-   Copyright 2022 TU Dresden (Paul Seibert as Scientific Employee)
+Copyright 10/2020 - 04/2021 Paul Seibert for Diploma Thesis at TU Dresden
+Copyright 05/2021 - 12/2021 TU Dresden (Paul Seibert as Scientific Assistant)
+Copyright 2022 TU Dresden (Paul Seibert as Scientific Employee)
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
+
 from __future__ import annotations
 
 import numpy as np
@@ -29,18 +30,16 @@ class FFTCrossCorrelations(PhaseDescriptor):
 
     @staticmethod
     def make_multiphase_descriptor(
-            desired_shape_2d=(64, 64), 
-            n_phases: int = None,
-            limit_to: int = None, 
-            **kwargs) -> callable:
+        desired_shape_2d=(64, 64), n_phases: int = None, limit_to: int = None, **kwargs
+    ) -> callable:
         include_auto = False
         mask = np.zeros(desired_shape_2d)
         i_center = desired_shape_2d[0] // 2
         j_center = desired_shape_2d[1] // 2
-        mask[i_center - limit_to + 1:i_center+limit_to, j_center - limit_to + 1:j_center+limit_to] = 1
+        mask[i_center - limit_to + 1 : i_center + limit_to, j_center - limit_to + 1 : j_center + limit_to] = 1
         mask = mask.astype(bool)
         mask = tf.constant(mask, dtype=tf.bool)
-        descriptor_shape = (limit_to * 2 - 1,) *  2
+        descriptor_shape = (limit_to * 2 - 1,) * 2
 
         # @tf.function
         # def compute_descriptor(microstructure: tf.Tensor) -> tf.Tensor:
@@ -65,14 +64,19 @@ class FFTCrossCorrelations(PhaseDescriptor):
                     ms_from_fourier = tf.signal.fft2d(ms_from)
                     ms_to = tf.cast(mg_input[0, :, :, phase_to], tf.complex128)
                     ms_to_fourier = tf.signal.fft2d(ms_to)
-                    fourier_coefficients = ms_from_fourier * tf.math.conj(ms_to_fourier) / tf.cast(tf.size(ms_from), tf.complex128)
+                    fourier_coefficients = (
+                        ms_from_fourier * tf.math.conj(ms_to_fourier) / tf.cast(tf.size(ms_from), tf.complex128)
+                    )
                     real_coefficients = tf.signal.ifft2d(fourier_coefficients)
                     real_coefficients = tf.signal.fftshift(real_coefficients)
-                    selected_components = tf.reshape(tf.boolean_mask(tf.cast(real_coefficients, tf.float64), mask), descriptor_shape)
+                    selected_components = tf.reshape(
+                        tf.boolean_mask(tf.math.real(real_coefficients), mask),
+                        descriptor_shape,
+                    )
                     correlation_list.append(selected_components)
             return tf.stack(correlation_list, axis=0)
-        return model
 
+        return model
 
 
 def register() -> None:
